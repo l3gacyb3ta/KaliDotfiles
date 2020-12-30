adee0c30422e5b837ba7711ae14e4fdec54097e4
l3gacy <l3gacy@DarkGalaxy.l3gacyhome>
2020-12-29 20:56:43 -0500

    Added i3-lock

---
+++ lock.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#1ce8f5'  # default
+T='#4db0b6'  # text
+W='#e3170a'  # wrong
+V='#bb00bbbb'  # verifying
+
+i3lock \
+--insidevercolor=$C   \
+--ringvercolor=$D     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$B      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--verifcolor=$T        \
+--wrongcolor=$T        \
+--timecolor=$T        \
+--datecolor=$T        \
+--layoutcolor=$T      \
+--keyhlcolor=$W       \
+--bshlcolor=$W        \
+\
+--screen 1            \
+--blur 5              \
+--clock               \
+--indicator           \
+--timestr="%H:%M:%S"  \
+--datestr="%A, %m %Y" \
+--keylayout 1         \
+
+# --veriftext="Drinking verification can..."
+# --wrongtext="Nope!"
+# --textsize=20
+# --modsize=10
+# --timefont=comic-sans
+# --datefont=monofur
+# etc
